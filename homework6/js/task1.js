bf22e99fe5694bf3d83c6bf65d6b38a683550551
Dmytro <dmitrodanileiko1@gmail.com>
2022-07-27 19:19:15 +0300

    style changes

--- homework6/js/task1.js
+++ homework6/js/task1.js
@@ -5,8 +5,10 @@ const b1 = parseInt(prompt('enter a1'));
 const b2 = parseInt(prompt('enter a1'));
 const c1 = parseInt(prompt('enter a1'));
 const c2 = parseInt(prompt('enter a1'));
+
 const mid1 = (a1 + b1) / 2;
 const mid2 = (a2 + b2) / 2;
+
 if (c1 === mid1 && c2 === mid2) {
   console.log(true);
 } else {
--- homework6/js/task2.js
+++ homework6/js/task2.js
@@ -2,6 +2,7 @@
 const a = parseInt(prompt('enter A'));
 const b = parseInt(prompt('enter B'));
 const c = parseInt(prompt('enter C'));
+
 if (a + b > c) {
   if (a === b && a === c) {
     console.log('Equivalent triangle.');
