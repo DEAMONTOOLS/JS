3355c925cb600b32c4a97b2042964008a63838f0
Dmytro <dmitrodanileiko1@gmail.com>
2022-08-01 12:17:11 +0300

    change variables name

--- homework7/src/js/task1.js
+++ homework7/src/js/task1.js
@@ -1,33 +1,33 @@
 // Your code goes here
 const email = prompt('Enter your email adress: ')
-const minLenEmail = 6
-const minLenPass = 5
+const minLengthEmail = 6
+const minLengthPassword = 5
 
 if (email === null || email === ''){
     alert('Canceled')
-} else if (email.length < minLenEmail){
+} else if (email.length < minLengthEmail){
     alert('I don\'t know any emails having name length less than 6 symbols')
 } else if (email === 'user@gmail.com') {  
-    let passwd = prompt('Enter your password: ')
+    let password = prompt('Enter your password: ')
 
     // check user password
-    if (passwd === 'UserPass'){
+    if (password === 'UserPass'){
         const change = confirm('Do you want change pass?')
 
         // check, did player want to change him password
         if (change){
-            let checkPass = prompt('Enter old pass')
+            let checkPassword = prompt('Enter old pass')
 
-            if (checkPass === passwd){
-                const newPass = prompt('Enter new pass')
+            if (checkPassword === password){
+                const newPassword = prompt('Enter new pass')
                 
                 // create new password
-                if (newPass.length > minLenPass ) {
-                    const newPasswd = prompt('Enter new pass')
+                if (newPassword.length > minLengthPassword ) {
+                    const checkNewPassword = prompt('Enter new pass')
 
                     // check new password by repeating second time
-                    if (newPasswd === newPass){ 
-                        passwd = newPasswd
+                    if (checkNewPassword === newPassword){ 
+                        password = checkNewPassword
                         alert('You have successfully changed your password.')
                     } else {
                         alert('Wrong pass')
@@ -37,7 +37,7 @@ if (email === null || email === ''){
                     alert('It’s too short password. Sorry.')
                 }
 
-            } else if (checkPass === null || checkPass === ''){
+            } else if (checkPassword === null || checkPassword === ''){
                 alert('Canceled')
             } else {
                 alert('Wrong password')
@@ -46,31 +46,31 @@ if (email === null || email === ''){
         } else {
             alert('You have failed the change.')
         }
-    } else if (passwd === null || passwd === ''){
+    } else if (password === null || password === ''){
         alert('Canceled')
     } else {
         alert('Wrong pass')
     }
 } else if (email === 'admin@gmail.com') {
-    let passwd = prompt('Enter your password: ')
+    let password = prompt('Enter your password: ')
 
     // check admin password
-    if (passwd === 'AdminPass'){
+    if (password === 'AdminPass'){
         const change = confirm('Do you want change pass?')
 
         // check, did player want to change him password
         if (change){
-            let checkPass = prompt('Enter old pass')
+            let checkPassword = prompt('Enter old pass')
 
-            if (checkPass === passwd){
-                const newPass = prompt('Enter new pass')
+            if (checkPassword === password){
+                const newPassword = prompt('Enter new pass')
 
                 // create new password
-                if (newPass.length > minLenPass) {
-                    const newPasswd = prompt('Enter new pass')
+                if (newPassword.length > minLengthPassword) {
+                    const checkNewPassword = prompt('Enter new pass')
 
-                    if (newPasswd === newPass){
-                        passwd = newPasswd
+                    if (checkNewPassword === newPassword){
+                        password = newPasswd
                         alert('You have successfully changed your password.')
                     } else {
                         alert('Wrong pass')
@@ -80,7 +80,7 @@ if (email === null || email === ''){
                     alert('It’s too short password. Sorry.')
                 }
 
-            } else if (checkPass === null || checkPass === ''){
+            } else if (checkPassword === null || checkPassword === ''){
                 alert('Canceled')
             } else {
                 alert('Wrong password')
@@ -89,7 +89,7 @@ if (email === null || email === ''){
         } else {
             alert('You have failed the change.')
         }
-    } else if (passwd === null || passwd === ''){
+    } else if (password === null || password === ''){
         alert('Canceled')
     } else {
         alert('Wrong pass')
