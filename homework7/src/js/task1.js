bc956e1fd07b74cc84af4631cf7904194911ad73
Dmytro <dmitrodanileiko1@gmail.com>
2022-08-03 12:39:36 +0300

    add semicolon

--- homework7/src/js/task1.js
+++ homework7/src/js/task1.js
@@ -1,99 +1,99 @@
 // Your code goes here
-const email = prompt('Enter your email adress: ')
-const minLengthEmail = 6
-const minLengthPassword = 5
+const email = prompt('Enter your email adress: ');
+const minLengthEmail = 6;
+const minLengthPassword = 5;
 
 if (email === null || email === ''){
-    alert('Canceled')
+    alert('Canceled');
 } else if (email.length < minLengthEmail){
-    alert('I don\'t know any emails having name length less than 6 symbols')
+    alert('I don\'t know any emails having name length less than 6 symbols');
 } else if (email === 'user@gmail.com') {  
-    let password = prompt('Enter your password: ')
+    let password = prompt('Enter your password: ');
 
     // check user password
     if (password === 'UserPass'){
-        const change = confirm('Do you want change pass?')
+        const change = confirm('Do you want change pass?');
 
         // check, did player want to change him password
         if (change){
-            let checkPassword = prompt('Enter old pass')
+            let checkPassword = prompt('Enter old pass');
 
             if (checkPassword === password){
-                const newPassword = prompt('Enter new pass')
+                const newPassword = prompt('Enter new pass');
                 
                 // create new password
                 if (newPassword.length > minLengthPassword ) {
-                    const checkNewPassword = prompt('Enter new pass')
+                    const checkNewPassword = prompt('Enter new pass');
 
                     // check new password by repeating second time
                     if (checkNewPassword === newPassword){ 
-                        password = checkNewPassword
-                        alert('You have successfully changed your password.')
+                        password = checkNewPassword;
+                        alert('You have successfully changed your password.');
                     } else {
-                        alert('Wrong pass')
+                        alert('Wrong pass');
                     }
 
                 } else {
-                    alert('It’s too short password. Sorry.')
+                    alert('It’s too short password. Sorry.');
                 }
 
             } else if (checkPassword === null || checkPassword === ''){
-                alert('Canceled')
+                alert('Canceled');
             } else {
-                alert('Wrong password')
+                alert('Wrong password');
             }
 
         } else {
-            alert('You have failed the change.')
+            alert('You have failed the change.');
         }
     } else if (password === null || password === ''){
-        alert('Canceled')
+        alert('Canceled');
     } else {
-        alert('Wrong pass')
+        alert('Wrong pass');
     }
 } else if (email === 'admin@gmail.com') {
-    let password = prompt('Enter your password: ')
+    let password = prompt('Enter your password: ');
 
     // check admin password
     if (password === 'AdminPass'){
-        const change = confirm('Do you want change pass?')
+        const change = confirm('Do you want change pass?');
 
         // check, did player want to change him password
         if (change){
-            let checkPassword = prompt('Enter old pass')
+            let checkPassword = prompt('Enter old pass');
 
             if (checkPassword === password){
-                const newPassword = prompt('Enter new pass')
+                const newPassword = prompt('Enter new pass');
 
                 // create new password
                 if (newPassword.length > minLengthPassword) {
-                    const checkNewPassword = prompt('Enter new pass')
+                    const checkNewPassword = prompt('Enter new pass');
 
                     if (checkNewPassword === newPassword){
-                        password = newPasswd
-                        alert('You have successfully changed your password.')
+                        password = newPasswd;
+                        alert('You have successfully changed your password.');
                     } else {
-                        alert('Wrong pass')
+                        alert('Wrong pass');
                     }
 
                 } else {
-                    alert('It’s too short password. Sorry.')
+                    alert('It’s too short password. Sorry.');
                 }
 
             } else if (checkPassword === null || checkPassword === ''){
-                alert('Canceled')
+                alert('Canceled');
             } else {
-                alert('Wrong password')
+                alert('Wrong password');
             }
 
         } else {
-            alert('You have failed the change.')
+            alert('You have failed the change.');
         }
     } else if (password === null || password === ''){
-        alert('Canceled')
+        alert('Canceled');
     } else {
-        alert('Wrong pass')
+        alert('Wrong pass');
     }
 } else {
-    alert('I don\'t know you')
+    alert('I don\'t know you');
 }
--- homework7/src/js/task2.js
+++ homework7/src/js/task2.js
@@ -1,15 +1,15 @@
 // Your code goes here
-const check = confirm('Do you want to play a game?')
-const maxCount = 3
-let running = true
-let winSum = 0
-let range = 9
+const check = confirm('Do you want to play a game?');
+const maxCount = 3;
+let running = true;
+let winSum = 0;
+let range = 9;
 let earn = 100;
 
 // running while player want to play
 while(running){
     if (check) {
-        alert('Lets play. You have 3 try')
+        alert('Lets play. You have 3 try');
         const num = Math.floor(Math.random() * range); // create random range
 
         // loop to check input number
@@ -19,35 +19,35 @@ while(running){
             //how much player earn if he guessed
             if (userNum == num){
                 if (count === 0){
-                    alert('Congratulation, you won')
+                    alert('Congratulation, you won');
                     winSum += earn;
-                    break
+                    break;
                 } else if (count === 1){
                     alert('Congratulation, you won')
                     winSum += earn / 2;
-                    break 
+                    break; 
                 } else { 
-                    alert('Congratulation, you won')
+                    alert('Congratulation, you won');
                     winSum += earn / 4;
-                    break
+                    break;
                 } 
             }
         }
 
-        running = confirm('Do you want to continue?')
+        running = confirm('Do you want to continue?');
 
         // if you want continue play the range and winning money will be incresed
         if (running){
-            range += 4
-            earn *= 2
+            range += 4;
+            earn *= 2;
         }
         else {
-            alert(`You win ${winSum}$`)
+            alert(`You win ${winSum}$`);
         }
     }
     else {
-        running = false // close game
-        alert('You did not become a billionaire, but can.')
+        running = false; // close game
+        alert('You did not become a billionaire, but can.');
     }
 
 }
